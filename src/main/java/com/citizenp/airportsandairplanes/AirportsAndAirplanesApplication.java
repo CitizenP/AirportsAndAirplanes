481a58bfc5ae1d3acc8f5cf66cc7658c8566c18d
placdo <placidomelo@gmail.com>
2023-01-01 11:53:23 +0000

    [TASK] Initial commit

---
+++ src/main/java/com/citizenp/airportsandairplanes/AirportsAndAirplanesApplication.java
@@ -0,0 +1,13 @@
+package com.citizenp.airportsandairplanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsAndAirplanesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportsAndAirplanesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citizenp/airportsandairplanes/AirportsAndAirplanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizenp.airportsandairplanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportsAndAirplanesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
